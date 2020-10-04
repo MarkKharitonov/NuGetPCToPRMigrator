dc63222a7ca772baac67a4e39b9cb02fc62b2f2c
Kharitonov, Mark <Mark.Kharitonov@ceridian.com>
2020-10-04 17:32:20 -0400

    Initial Commit

---
+++ NuGetPCToPRMigrator/Program.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using HWND = System.IntPtr;
+
+namespace Ceridian
+{
+    public static class Program
+    {
+        private const string DEVENV = @"C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\IDE\devenv.exe";
+        private const string DEFAULT_BROWSER = "chrome.exe";
+    
+        private delegate bool EnumWindowsProc(HWND hWnd, int lParam);
+
+        [DllImport("USER32.DLL")]
+        private static extern bool EnumWindows(EnumWindowsProc enumFunc, int lParam);
+
+        [DllImport("USER32.DLL")]
+        private static extern int GetWindowText(HWND hWnd, StringBuilder lpString, int nMaxCount);
+
+        [DllImport("USER32.DLL")]
+        private static extern int GetWindowTextLength(HWND hWnd);
+
+        [DllImport("USER32.DLL")]
+        private static extern bool IsWindowVisible(HWND hWnd);
+
+        [DllImport("USER32.DLL")]
+        private static extern HWND GetShellWindow();
+
+        [DllImport("user32.dll")]
+        private static extern bool SetForegroundWindow(HWND hWnd);
+
+        public static HWND GetDialogHandle()
+        {
+            HWND shellWindow = GetShellWindow();
+            HWND handle = HWND.Zero;
+
+            EnumWindows(delegate (HWND hWnd, int lParam)
+            {
+                const string TITLE_PREFIX = "Migrate NuGet format to PackageReference - ";
+
+                if (hWnd == shellWindow || !IsWindowVisible(hWnd))
+                {
+                    return true;
+                }
+
+                int length = GetWindowTextLength(hWnd);
+                if (length < TITLE_PREFIX.Length + 1)
+                {
+                    return true;
+                }
+
+                var builder = new StringBuilder(length);
+                GetWindowText(hWnd, builder, length + 1);
+
+                var title = builder.ToString();
+                if (title.StartsWith(TITLE_PREFIX))
+                {
+                    handle = hWnd;
+                    return false;
+                }
+                return true;
+            }, 0);
+
+            return handle;
+        }
+
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine($"Usage: {Process.GetCurrentProcess().ProcessName} SlnFile1 SlnFile2 ...");
+                return;
+            }
+
+            foreach (var slnFilePath in args)
+            {
+                Process devenv = null;
+                var dte = RunningVSInstanceFinder.Find(slnFilePath);
+                if (dte == null)
+                {
+                    devenv = Process.Start(DEVENV, slnFilePath);
+                    do
+                    {
+                        Thread.Sleep(1000);
+                        dte = RunningVSInstanceFinder.Find(slnFilePath);
+                    }
+                    while (dte == null);
+                }
+                MigrateSolution(dte, slnFilePath);
+                if (devenv != null)
+                {
+                    devenv.Kill();
+                }
+            }
+        }
+
+        private static T ExecuteWithRetry<T>(Func<T> func)
+        {
+            int retryCount = 30;
+            for (; ; )
+            {
+                try
+                {
+                    return func();
+                }
+                catch
+                {
+                    --retryCount;
+                    if (retryCount == 0)
+                    {
+                        throw;
+                    }
+                    Thread.Sleep(1000);
+                }
+            }
+        }
+
+        private static string StartProjectMigration(EnvDTE80.DTE2 dte, int i, int total, string solutionName, string projectFilePath)
+        {
+            Console.WriteLine($"[{i}/{total}] {projectFilePath}");
+            var projectName = Path.GetFileNameWithoutExtension(projectFilePath);
+            var projectItem = dte.ToolWindows.SolutionExplorer.GetItem($@"{solutionName}\{projectName}");
+            projectItem.Select(EnvDTE.vsUISelectionType.vsUISelectionTypeSelect);
+            dte.ExecuteCommand("ClassViewContextMenus.ClassViewProject.Migratepackages.configtoPackageReference");
+            return projectName;
+        }
+        private static T WaitForResult<T>(Func<T> func, Predicate<T> isReady)
+        {
+            T res;
+            while (!isReady(res = func()))
+            {
+                Thread.Sleep(1000);
+            }
+            return res;
+        }
+
+        private static object InitializeNuGetPackageManager(EnvDTE80.DTE2 dte)
+        {
+            dte.ExecuteCommand("View.PackageManagerConsole");
+            return null;
+        }
+
+        private static string TryReadAllText(string filePath)
+        {
+            try
+            {
+                return File.ReadAllText(filePath);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        private static Func<string, string> GetHtmlReportFilePathFunc(string slnFilePath)
+        {
+            var migrationBackupFolderPath = $@"{slnFilePath}\..\MigrationBackup";
+            HashSet<string> done;
+            if (Directory.Exists(migrationBackupFolderPath))
+            {
+                done = Directory
+                    .EnumerateDirectories(migrationBackupFolderPath)
+                    .ToHashSet(StringComparer.OrdinalIgnoreCase);
+            }
+            else
+            {
+                done = new HashSet<string>(StringComparer.OrdinalIgnoreCase);
+            }
+
+            return projectName => Directory.Exists(migrationBackupFolderPath) ? Directory
+                .EnumerateDirectories(migrationBackupFolderPath)
+                .Where(path => !done.Contains(path) && File.Exists($@"{path}\{projectName}\NuGetUpgradeLog.html"))
+                .Select(path => $@"{path}\{projectName}\NuGetUpgradeLog.html")
+                .FirstOrDefault() : null;
+        }
+
+        private static object SaveAll(EnvDTE80.DTE2 dte)
+        {
+            dte.ExecuteCommand("File.SaveAll");
+            Thread.Sleep(5000);
+            return null;
+        }
+
+        private static void MigrateSolution(EnvDTE80.DTE2 dte, string slnFilePath)
+        {
+            var getHTMLReportFilePath = GetHtmlReportFilePathFunc(slnFilePath);
+
+            var solutionName = Path.GetFileNameWithoutExtension(slnFilePath);
+
+            ExecuteWithRetry(() => InitializeNuGetPackageManager(dte));
+            var projects = ExecuteWithRetry(() => dte.Solution.Projects.Cast<EnvDTE.Project>().Where(IsClassLibraryWithPackagesConfig).ToList());
+            int i = 0;
+            foreach (var project in projects)
+            {
+                ++i;
+                var projectFullName = ExecuteWithRetry(() => project.FullName);
+                var projectName = ExecuteWithRetry(() => StartProjectMigration(dte, i, projects.Count, solutionName, projectFullName));
+                var handle = WaitForResult(GetDialogHandle, h => h != HWND.Zero);
+
+                SetForegroundWindow(handle);
+                SendKeys.SendWait("{ENTER}");
+
+                int j = 0;
+                var packagesConfigFilePath = $@"{projectFullName}\..\packages.config";
+                string htmlReportFilePath = WaitForResult(() => getHTMLReportFilePath(projectName), path => path != null || ++j >= 60 && !File.Exists(packagesConfigFilePath));
+
+                if (htmlReportFilePath != null)
+                {
+                    string htmlReport = WaitForResult(() => TryReadAllText(htmlReportFilePath), content => content != null);
+                    Process.Start("taskkill", "/f /im " + DEFAULT_BROWSER);
+                    if (htmlReport.Contains("No issues were found."))
+                    {
+                        Directory.Delete($@"{htmlReportFilePath}\..\..", true);
+                    }
+                }
+            }
+            ExecuteWithRetry(() => SaveAll(dte));
+        }
+
+        private static bool IsWebApplication(EnvDTE.Project project) =>
+            project.Properties.Cast<EnvDTE.Property>().Any(p => p.Name == "WebApplication.UseIISExpress");
+
+        private static bool IsClassLibraryWithPackagesConfig(EnvDTE.Project p) =>
+            p.Kind == "{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}" &&
+            File.Exists($@"{p.FullName}\..\packages.config") &&
+            !IsWebApplication(p);
+    }
+}
---
+++ README.md
@@ -0,0 +1,63 @@
+# Introduction
+This tool migrates all the non Asp.Net projects in the given solution from packages.config (PC) to PackageReferences (PR) using the [Migration tool][3] built-in in Visual Studio 2017 and up.
+
+That [Migration tool][3] is fantastic if you need to convert one or two projects, but totally useless when the solution contains a hundred or so of projects. I was unable to find any good way to [migrate en masse][6] and hence wrote my own something, which still leverages the built-in facility.
+
+Given a solution file path it proceeds like this:
+ 1. Locates the Visual Studio instance where the solution is open. If not found a new VS instance is open with the solution in question.
+ 2. Obtains the [DTE2][2] object with the help of [RunningVSInstanceFinder][1].
+ 3. Using the [DTE2][2] object it enumerates all the non Asp.Net projects and for each of them runs the [Migration tool][3].
+ 4. At the end when all the relevant projects were processed and if the tool has previously open a new VS instance - the tool will close that VS instance.
+
+The code is smart enough to:
+ - Close the modal dialog (open by the migration tool) using the [SendKeys.SendWait][5] API.
+ - Properly initialize the NuGet Package Manager to address the [known issue][4].
+ - Absorb `COMExceptions` occurring when trying to automate the IDE while it is busy doing something.
+ - Retry operations failed due to IDE being busy.
+ - Identify when a project migration is done by checking the presence of the migration report html.
+
+The code is NOT smart enough to:
+ - Figure out the devenv path. Right now it hard codes it to **C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\IDE\devenv.exe**
+ - Figure out the default browser. Right now it hard codes it to `chrome.exe`. Why does it care in the first place? See [below](#kill_browser).
+
+# Building
+The code consumes [RunningVSInstanceFinder][1] as a NuGet package, which does not exist publicly. So, trying to clone and build would not work. Alas, I do not have time to setup an external build pipeline for [RunningVSInstanceFinder][1] and so if you wish to use this tool you need to do the following:
+ 1. Clone [RunningVSInstanceFinder][1]
+ 2. Clone this repository
+ 3. Include the RunningVSInstanceFinder project into the solution of this tool.
+ 4. Modify the project of this tool to reference the `RunningVSInstanceFinder` as a project rather than as a Nuget library.
+ 5. Update the DEVENV constant in Program.cs to point to your devenv.exe
+ 6. Update the DEFAULT_BROWSER constant in Program.cs to point to your default browser.
+
+# Running
+Run the tool with a single argument - the solution file path. The tool may open a new VS instance, if it could not find an already running one with the solution in question already open there. Note, that a VS instance that presents a modal dialog cannot be automated.
+
+The tool is going to bring forward the relevant VS instance and you will not be able to do anything while it is running, because it would constantly steal the mouse focus from you.
+
+I tried it on a solution containing more than a hundred projects, seems to work fine. It is possible that the tool would exhaust it retry capacity (should VS become really busy) and abort. It is safe to restart, optionally killing the VS instance.
+
+Here how it looks like on a small solution:
+```powershell
+C:\tools\NuGetPCToPRMigrator> .\NuGetPCToPRMigrator\bin\Debug\net472\NuGetPCToPRMigrator.exe C:\dayforce\tip\build\deployer.sln
+[1/5] C:\Dayforce\tip\Build\2010\Activities\ActivityPack.csproj
+[2/5] C:\Dayforce\tip\Build\2010\Deployer\Deployer.csproj
+[3/5] C:\Dayforce\tip\Build\2010\Configurator\Configurator.csproj
+[4/5] C:\Dayforce\tip\Build\2010\DeploymentEngine\DeploymentEngine.csproj
+[5/5] C:\Dayforce\tip\Build\2010\DeployerTests\DeployerTests.csproj
+C:\tools\NuGetPCToPRMigrator>
+```
+The solution contains more than 5 projects, but only 5 contain packages.config and are non Asp.Net projects.
+
+## <a name="kill_browser"></a>Killing all the default browser instances
+The [Migration tool][3] opens an HTML report at the end of a migration. For a solution with 100 eligible projects that would result in a 100 tabs in the default browser. That is why the code kills the default browser at the end of each migration.
+
+# Potential Issues
+ - VS automation is fragile. It may sometimes fail on a large solution somewhere midway. Just restart the tool.
+ - The tool saves all files in the solution and waits 5 seconds. May not be enough for a big solution.
+
+[1]: https://github.com/MarkKharitonov/RunningVSInstanceFinder
+[2]: https://docs.microsoft.com/en-us/dotnet/api/envdte80.dte2?view=visualstudiosdk-2019
+[3]: https://devblogs.microsoft.com/nuget/migrate-packages-config-to-package-reference/
+[4]: https://docs.microsoft.com/en-us/nuget/consume-packages/migrate-packages-config-to-package-reference#issue
+[5]: https://docs.microsoft.com/en-us/dotnet/api/system.windows.forms.sendkeys.sendwait?view=netcore-3.1
+[6]: https://stackoverflow.com/questions/64163515/migrating-multiple-large-solutions-to-packagereference-en-masse
